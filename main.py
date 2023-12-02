9e335a76bffce0d8e7f39b98741de2919cc0808d
Mrmushroomsquid1 <147305067+Mrmushroomsquid1@users.noreply.github.com>
2023-12-03 10:09:48 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,103 @@
+ 
+print("Welcome to my quiz")
+Name = input("What's your name? ")
+print("Welcome " + Name )
+
+playing = input("Do you want to play? ")
+
+if playing != "Yes":
+    print("Okay. Bye")
+    exit()
+
+print("Okay! Lets get started")
+
+answerQ1 = input("What is the closest planet to the sun? ")
+
+if answerQ1 != "Mercury":
+    print("Wrong answer. The correct answer was mercury.`")
+    exit()
+
+print("Nice job! You got is correct")
+
+answerQ2 = input("What is the most popular programing language? ")
+
+if answerQ2 != "Python":
+    print("Wrong answer. The correct answer was python.")
+    exit()
+
+print("Your doing great")
+
+answerQ3 = input("What does E equal in the equation E=mc2? ")
+
+if answerQ3 != "Energy":
+    print("Wrong answer. The correct answer way energy.")
+    exit()
+
+print("Great job. The questions are going to get harder now :)")
+
+answerQ4 = input("What does SSD stand for in the context of computors? ")
+
+if answerQ4 != "Solid state drive":
+    print("Wrong answer. The correct answer was solid state drive")
+    exit()
+
+print("Nice job")
+
+answerQ5 = input("What element is number 2 on the periodic table? ")
+
+if answerQ5 != "Helium":
+    print("Wrong answer. The correct answer was Helium")
+    exit()
+
+print("Its getting harder")
+
+answerQ5 = input("What is the most popular game engine in the world? ")
+
+if answerQ5 != "Unity":
+    print("Wrong answer. The correct answer was Unity")
+    exit()
+
+print("Good job, your half way there")
+
+answerQ6 = input("What is the second fastest land animal? ")
+
+if answerQ6 != "Greyhound":
+    print("Wrong answer. The correct answer was greyhound")
+    exit()
+
+print("Your doing well!")
+
+answerQ7 = input("Who won the 1976 Nba finals? ")
+
+if answerQ7 != "Boston Celtics":
+    print("Wrong answer. The correct answer was the boston celtics")
+    exit()
+
+print("Your good at this!")
+
+answerQ8 = input("What does RAM stand for in the context of computors? ")
+
+if answerQ8 != "Random access memory":
+    print("Wrong answer. The correct answer was random access memory")
+    exit()
+
+print("Almost there")
+
+answerQ9 = input("Who composed the song symphony number 7? ")
+
+if answerQ9 != "Beethoven":
+    print("Wrong answer. The correct answer was Beethoven")
+    exit()
+
+answerQ10 = input("Who got traded to the phoenix suns during the 1996 offseason? ")
+
+if answerQ10 != "Charles Barkly":
+    print("Wrong answer. The correct answer was Charles Barkly.")
+    exit()
+
+print("Congradulations, you beat the quiz <(""<) :) ")
+
+#add ui
+
+
+
